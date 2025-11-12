edca1e1297ade936aa4a1f5761d94f1f41f31622
Jayraj Surti <jayrajsurti@Jayrajs-MacBook-Pro.local>
2025-11-12 16:50:43 +0000

    Add favicon for ios

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,15 @@ export const metadata: Metadata = {
   title: 'Meghna Mangat',
   description:
     'A portfolio website showcasing projects, skills, and experiences.',
+  icons: {
+    apple: [
+      {
+        url: '/icons/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
